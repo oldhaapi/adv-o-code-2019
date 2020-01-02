96857df245b2d223f05fef01c4b7bfe22523395b
Clay Haapala <clay@haapi.mn.org>
2020-01-01 21:30:13 -0600

    Day 6 cribbed from reddit
    I used a reddit solution that also used tree-seq, as I was planning to do.

--- day6/src/day6/core.clj
+++ day6/src/day6/core.clj
@@ -1,24 +1,58 @@
 (ns day6.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
 
-(defn read-orbits
-  "Read a)b strings from specified file"
-  [fname]
-  (println "opening:" fname)
-  (with-open [rdr (clojure.java.io/reader fname)]
-    (map #(seq (clojure.string/split % #"\)")) (doall (line-seq rdr)))))
+;; this is from a solution found on reddit
 
-(defn -main
-  "Day6 - count nodes in nested orbital vectors"
-  [& args]
-  (let [infile (if (nil? args) "orbits.txt" (first args))
-        orbits-seq-orig (read-orbits infile)
-        orb-com (filter #(= "COM" (first %)) orbits-seq-orig)
-        orb-seq (concat orb-com (filter #(not= "COM" (first %)) orbits-seq-orig))
-        orb-tree (tree-seq seq? identity orb-seq)]
-    (println "orb-com:" orb-com)
-    (println "Input" (count orb-seq) "orbits")
-    (if (not (nil? args)) (println "orb-seq:\n" orb-seq))
-    (println (count orb-tree))
-    (-> orb-tree distinct count println)
-    (if (not (nil? args)) (distinct orb-tree))))
+(defn format-input [input]
+  (->> (str/split (str/trim input) #"\n")
+       (map str/trim)
+       (map #(str/split % #"\)"))))
+
+(defn connected-nodes [n edges]
+  (->> (filter (fn [[x _]] (= x n)) edges)
+       (map second)))
+
+(defn build-graph
+  ([edges] (build-graph edges "COM"))
+  ([edges start-node]
+   (when-let [nodes (connected-nodes start-node edges)]
+     [start-node  (mapv (partial build-graph edges) nodes)])))
+
+(def input (slurp "orbits.txt"))
+
+(defn solve-1 []
+  (->> input
+       format-input
+       build-graph
+       (tree-seq second second)
+       (map flatten)
+       (map #(-> % count dec))
+       (reduce +)))
+
+(defn length-of-longest-path-containing-a-and-not-b [a b paths]
+  (->> (map set paths)
+       (filter #(% a))
+       (remove #(% b))
+       (sort-by >)
+       count
+       dec))
+
+(defn distance-between [node-a node-b tree-s]
+  (let [flat-paths (map flatten tree-s)]
+    (+ (length-of-longest-path-containing-a-and-not-b
+        node-a node-b flat-paths)
+       (length-of-longest-path-containing-a-and-not-b
+        node-b node-a flat-paths))))
+
+(defn solve-2 []
+  (->> input
+       format-input
+       build-graph
+       (tree-seq second second)
+       (distance-between "YOU" "SAN")))
+
+
+(defn -main []
+  (println "Solve-1:" (solve-1))
+  (println "Solve-2:" (solve-2)))
+
